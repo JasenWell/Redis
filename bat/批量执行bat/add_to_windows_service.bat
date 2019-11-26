68af4b44c7013ce1aceef53b9c5c9e003c90c7a7
hejinhui <409762605@qq.com>
2019-11-27 00:19:08 +0800

    批量处理脚本
    批量执行多个bat

---
+++ bat/批量执行bat/add_to_windows_service.bat
@@ -0,0 +1,9 @@
+﻿rem start work!
+:: 不会显示的注释  rem注释会回显
+:: master_6379为脚本同目录对应的redis文件夹名
+::cd master_6379
+::add_to_windows_service.bat 
+@echo off
+start /D "D:\Sever\redis\master_6379" add_to_windows_service.bat
+start /D "D:\Sever\redis\slave_6380" add_to_windows_service.bat
+start /D "D:\Sever\redis\slave_6381" add_to_windows_service.bat
---
+++ bat/批量执行bat/startup.bat
@@ -0,0 +1,9 @@
+﻿rem start work!
+:: 不会显示的注释  rem注释会回显
+:: master_6379为脚本同目录对应的redis文件夹名
+::cd master_6379
+::startup.bat
+
+start /D "D:\Sever\redis\master_6379" startup.bat
+start /D "D:\Sever\redis\slave_6380" startup.bat
+start /D "D:\Sever\redis\slave_6381" startup.bat
